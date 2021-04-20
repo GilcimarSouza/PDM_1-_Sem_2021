0a1abd68133060a36c63114cf2674acfc73db5d8
Pedro Chaves <pedroaqchaves@gmail.com>
2021-04-20 13:25:19 -0300

    App funcional

--- app/src/main/java/br/iesb/mobile/foodie_app/ui/fragment/MainFragment.kt
+++ app/src/main/java/br/iesb/mobile/foodie_app/ui/fragment/MainFragment.kt
@@ -1,38 +1,45 @@
 package br.iesb.mobile.foodie_app.ui.fragment
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import br.iesb.mobile.foodie_app.R
+import br.iesb.mobile.foodie_app.databinding.FragmentMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class MainFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        GlobalScope.launch(context = Dispatchers.Main) {
+            delay(2000)
+
+            try {
+                findNavController().navigate(R.id.action_mainFragment_to_onBoardingFragment)
+            } catch(e: Exception){
+                Toast.makeText(activity, "EROOOOOU", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        return binding.root
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val handler = Handler()
-        handler.postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
-           activity.finish()
-        }, 2000)
-    }
-
 
 }
--- app/src/main/java/br/iesb/mobile/foodie_app/ui/fragment/onboarding/screen/OnboardingFirstFragment.kt
+++ app/src/main/java/br/iesb/mobile/foodie_app/ui/fragment/onboarding/screen/OnboardingFirstFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import br.iesb.mobile.foodie_app.R
-import br.iesb.mobile.foodie_app.databinding.FragmentOnBoardingBinding
 import br.iesb.mobile.foodie_app.databinding.FragmentOnboardingFirstBinding
 
 class OnboardingFirstFragment : Fragment() {
