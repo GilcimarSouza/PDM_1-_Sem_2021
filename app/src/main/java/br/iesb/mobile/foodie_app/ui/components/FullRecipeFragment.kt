cab15df445e50d45c7827b6520d7fe06b94a2171
Pedro Chaves <pedroaqchaves@gmail.com>
2021-06-15 18:19:39 -0300

    Feita tela da receita inteira. Está instável a nevagação para ela.

--- app/src/main/java/br/iesb/mobile/foodie_app/ui/components/FullRecipeFragment.kt
+++ app/src/main/java/br/iesb/mobile/foodie_app/ui/components/FullRecipeFragment.kt
@@ -5,26 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import br.iesb.mobile.foodie_app.R
-import kotlinx.android.synthetic.main.fragment_view_receita.*
 
 
-class ViewReceitaFragment : Fragment() {
+class FullRecipeFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_receita, container, false)
+        return inflater.inflate(R.layout.fragment_full_recipe, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        iv_pao.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_fullRecipeFragment)
-        }
-
     }
+
 }
--- app/src/main/java/br/iesb/mobile/foodie_app/ui/fragment/apprunning/NewRecipeFragment.kt
+++ app/src/main/java/br/iesb/mobile/foodie_app/ui/fragment/apprunning/NewRecipeFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isEmpty
+import androidx.navigation.fragment.findNavController
 import br.iesb.mobile.foodie_app.R
 import br.iesb.mobile.foodie_app.databinding.FragmentNewRecipeBinding
 import br.iesb.mobile.foodie_app.databinding.FragmentTopRecipesBinding
+import kotlinx.android.synthetic.main.fragment_new_recipe.*
 
 class NewRecipeFragment : Fragment() {
 
@@ -27,6 +31,15 @@ class NewRecipeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        bt_ok_new_recipe.setOnClickListener {
+            if(textInputLayout.isEmpty() || textInputLayout3.isEmpty() || textInputLayout4.isEmpty()) {
+                Toast.makeText(activity, "Você deixou algum campo em branco!", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(activity, "Você Publicou uma nova receita!", Toast.LENGTH_LONG).show()
+                findNavController().navigate(R.id.action_newRecipeFragment_to_profileFragment)
+            }
+        }
 
     }
 
